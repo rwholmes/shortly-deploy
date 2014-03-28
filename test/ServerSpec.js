b96acb44830642a219bbb601194f294c7aefdffe
Hack Reactor Students <accounts@hackreactor.com>
2014-03-27 17:14:14 -0700

    working mongo!

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -3,7 +3,7 @@ var express = require('express');
 var expect = require('chai').expect;
 var app = require('../server-config.js');
 
-var db = require('../app/config');
+var db = require('../db/config');
 var User = require('../app/models/user');
 var Link = require('../app/models/link');
 
